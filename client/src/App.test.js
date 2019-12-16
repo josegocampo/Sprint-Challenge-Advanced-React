4773401bc500ceaab3224c386da8055e6004e004
josegocampo <jose.ocampo@mail.udp.cl>
2019-12-16 00:18:55 -0300

    finalized MVP

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,40 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
+import Players from './components/Players'
+import DarkMode from './hooks/DarkMode';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+afterEach(rtl.cleanup);
+
+
+test('Render the app', () => {
+  const wrapper = rtl.render(<Players/>)
+  const element = wrapper.queryByText(/morgan/i)
+  // expect(element).toBeVisible()
+})
+
+
+test('Check DarkMode', () => {
+const wrapper = rtl.render(<App/>)
+const element = wrapper.queryByText(/dark/i)
+  expect(element).toBeVisible()
+
+})
+
+
+test('Check Lloyd', () => {
+  const wrapper = rtl.render(<Players/>)
+  const element = wrapper.queryByText(/lloyd/i)
+})
+
+
+
+test('Check numbers', () => {
+  const wrapper = rtl.render(<Players/>)
+  const element = wrapper.queryByText(/number 87/i)
+})
+
+
+
